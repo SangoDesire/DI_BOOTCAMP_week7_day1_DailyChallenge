51ae5547db86bdfb04fae7f110a1877984868a91
Sango <analysteprodez.com>
2023-02-15 10:47:53 +0000

    week7 day1 daily challenge

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Chien chien = new Chien();
+        chien.crier();
+        chien.seDeplacer();
+        chien.seNourrir();
+    }
+}
